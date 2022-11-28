f85c569015d91b54461c197f3926cebdcb86cf69
claytonCarter <cgc1101ri@gmail.com>
2022-11-28 14:32:56 -0500

    second pop constructor

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void writePopToFile(population pop, String filename) throws IOException {
+        FileWriter fw = new FileWriter(filename);
+        ArrayList<organism[]> generations = pop.getGenerations();
+        for (int genIndex = 0; genIndex < generations.size(); genIndex++) {
+            fw.write("Generation: " + Integer.toString(genIndex) +"\n");
+            for (int orgIndex = 0; orgIndex < generations.get(genIndex).length; orgIndex++) {
+                
+                fw.write("Organism: " + Integer.toString(orgIndex) + "\nChromosome A\n");
+
+                String[] chromA = generations.get(genIndex)[orgIndex].getChrA();
+                for (int snpIndex = 0; snpIndex < chromA.length; snpIndex++) {
+                    fw.write(chromA[snpIndex] + ", ");
+                }
+
+                fw.write("\nChromosome B\n");
+
+                String[] chromB = generations.get(genIndex)[orgIndex].getChrB();
+                for (int snpIndex = 0; snpIndex < chromB.length; snpIndex++) {
+                    fw.write(chromB[snpIndex] + ", ");
+                }
+            }
+        }
+        fw.close();
+    }
+}
+
--- population.java
+++ population.java
@@ -10,7 +10,7 @@ public class population{
     ArrayList<organism[]> generations = new ArrayList<organism[]>() ;
     //creates a random population and simulates evoltion perhaps with some analysis methods 
     public population(int n,int m){//number of organisms n, chromosome size m
-        this.possibleSnps = initSnps(2*m); // initialize random snps for this population
+        possibleSnps = initSnps(2*m); // initialize random snps for this population
         this.randomFirstGeneration = new organism[n]; //main population for instance
         snp.expressedSnp[] expressions = new snp.expressedSnp[2*m]; //array for expressed snps
         int choice = rand.nextInt(100); //expression index
@@ -23,6 +23,10 @@ public class population{
         }
         this.generations.add(randomFirstGeneration);
     } 
+
+    public population(organism[] foundingPop){
+        this.generations.add(foundingPop);
+    }
 
 
     private snp[] initSnps(int m){ //create array of random snps (length 2m)
@@ -58,19 +62,12 @@ public class population{
         return nextGen;
     }
     
-    public void reproduce(int gens){ //simulate multiple generations of reproduction 
+    // public void reproduce(int gens){ //simulate multiple generations of reproduction 
 
-    }
-
-    // public organism[] getPop(){
-    //     return this.organismPop;
     // }
 
-    public static void main(String[] args){
-        organism[] generation1;
-        population testPop = new population(10,5); 
-        generation1 = testPop.newGeneration(5);
-        System.out.println(Arrays.toString(generation1));
-    
+    public ArrayList<organism[]> getGenerations() {
+        return generations;
     }
 }
+    
